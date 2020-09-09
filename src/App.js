ad91572da90aa524905e12874af51ebb441d61a8
rebeca-august <rebeca.cazacu@gmail.com>
2020-09-09 12:07:56 +0100

    Add recipe component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Recipe from "./components/Recipe";
+import heart from "./assets/heart.png";
+
+import styles from './App.module.css';
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("choco");
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search.trim());
+  };
+
+  const APP_ID = "22961b0c";
+  const APP_KEY = "267a8479f969f9b1474fec115bfd119c";
+
+
+  useEffect(() => {
+    const getRecipes = async () => {
+      setIsLoading(true);
+      const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+      const data = await response.json();
+
+      setRecipes(data.hits);
+      setIsLoading(false);
+    };
+    if (query) {
+      getRecipes();
+    }
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <form onSubmit={getSearch} className={styles.searchForm}>
+        <img className={styles.heart} src={heart} alt="" />
+        <input className={styles.searchBar} value={search} onChange={updateSearch} required />
+        <button className={styles.searchButton} disabled={isLoading}>{isLoading ? "Loading..." : "Search"}</button>
+      </form>
+
+      <div className={styles.recipes}>
+        {recipes.map(data => (
+          <Recipe
+            key={data.recipe.label}
+            title={data.recipe.label}
+            ingredients={data.recipe.ingredients}
+            image={data.recipe.image}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const Router = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/likedRecipes">
+        <h1>LikedRecipes</h1>
+        <Link to="/">Home</Link>
+      </Route>
+      <Route path="/">
+        <Link to="/likedRecipes">Liked recipes</Link>
+        <App />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );
