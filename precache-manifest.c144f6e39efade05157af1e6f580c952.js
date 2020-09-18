2f6d7ec0072deb8948c36eb978d6a9f189a90d20
rebeca-august <rebeca.cazacu@gmail.com>
2020-09-18 17:49:34 +0100

    Updates

---
+++ precache-manifest.c144f6e39efade05157af1e6f580c952.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f44178d549182012f59e3b0c9957dee",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "142ddc52f3ecd6e3a8b7",
+    "url": "/recipe-app/static/css/main.7b03c880.chunk.css"
+  },
+  {
+    "revision": "5e10c81eea37bc3afbcb",
+    "url": "/recipe-app/static/js/2.e4cb0b5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.e4cb0b5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "142ddc52f3ecd6e3a8b7",
+    "url": "/recipe-app/static/js/main.f57e1c12.chunk.js"
+  },
+  {
+    "revision": "cd77bb1956273d314d45",
+    "url": "/recipe-app/static/js/runtime-main.14680ac4.js"
+  },
+  {
+    "revision": "2138dcd2cf8f8dc340e3aff933d71e4f",
+    "url": "/recipe-app/static/media/heart.2138dcd2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rebeca-august/recipe-app/precache-manifest.8144f78b75083b0e865a37e3f55ee182.js"
+  "/recipe-app/precache-manifest.c144f6e39efade05157af1e6f580c952.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rebeca-august/recipe-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/recipe-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
