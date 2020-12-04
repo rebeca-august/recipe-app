7d742c4ca89f11da6a53dfdb5d4cbd742ffac351
rebeca-august <rebeca.cazacu@gmail.com>
2020-12-04 15:18:08 +0000

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,77 +1,77 @@
-import React, { useState, useEffect } from "react";
-import Recipe from "./components/Recipe";
-import Header from "./components/Header";
+import React, { useState, useEffect } from 'react'
+import Recipe from './components/Recipe'
+import Header from './components/Header'
 
-import styles from "./App.module.css";
+import styles from './App.module.css'
 
 const getFavoriteRecipes = () =>
-  JSON.parse(localStorage.getItem("favoriteRecipes")) || [];
+  JSON.parse(localStorage.getItem('favoriteRecipes')) || []
 
 function App() {
-  const APP_ID = "22961b0c";
-  const APP_KEY = "267a8479f969f9b1474fec115bfd119c";
+  const APP_ID = '22961b0c'
+  const APP_KEY = '267a8479f969f9b1474fec115bfd119c'
 
-  const [recipes, setRecipes] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("choco");
-  const [showFavorite, setShowFavorite] = useState(false);
+  const [recipes, setRecipes] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('choco')
+  const [showFavorite, setShowFavorite] = useState(false)
 
   const updateSearch = (e) => {
-    setSearch(e.target.value);
-  };
+    setSearch(e.target.value)
+  }
 
   const getSearch = (e) => {
-    e.preventDefault();
-    setQuery(search.trim());
-  };
+    e.preventDefault()
+    setQuery(search.trim())
+  }
 
   useEffect(() => {
     const getRecipes = async () => {
-      setIsLoading(true);
+      setIsLoading(true)
       const response = await fetch(
-        `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
-      );
-      const data = await response.json();
-      const favRecipes = getFavoriteRecipes();
+        `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`,
+      )
+      const data = await response.json()
+      const favRecipes = getFavoriteRecipes()
       const recipesData = data.hits.map((recipeData) => {
         const hasFavRecipe = favRecipes.find(
-          (favRecipe) => favRecipe.recipe.label === recipeData.recipe.label
-        );
+          (favRecipe) => favRecipe.recipe.label === recipeData.recipe.label,
+        )
         if (hasFavRecipe) {
-          return { ...recipeData, bookmarked: true };
+          return { ...recipeData, bookmarked: true }
         }
-        return recipeData;
-      });
+        return recipeData
+      })
 
-      setRecipes(recipesData);
-      setIsLoading(false);
-    };
-    if (query) {
-      getRecipes();
+      setRecipes(recipesData)
+      setIsLoading(false)
     }
-  }, [query]);
+    if (query) {
+      getRecipes()
+    }
+  }, [query])
   const updateLocalStorage = (newRecipes) => {
-    const favoriteRecipes = newRecipes.filter((recipe) => recipe.bookmarked);
-    localStorage.setItem("favoriteRecipes", JSON.stringify(favoriteRecipes));
-  };
+    const favoriteRecipes = newRecipes.filter((recipe) => recipe.bookmarked)
+    localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes))
+  }
 
   const toggleFavorite = (title) => {
     const updatedRecipes = recipes.map((data) => {
       if (data.recipe.label === title) {
-        return { ...data, bookmarked: !data.bookmarked };
+        return { ...data, bookmarked: !data.bookmarked }
       }
-      return data;
-    });
-    setRecipes(updatedRecipes);
-    updateLocalStorage(updatedRecipes);
-  };
+      return data
+    })
+    setRecipes(updatedRecipes)
+    updateLocalStorage(updatedRecipes)
+  }
 
   const toggleFavoriteList = () => {
-    setShowFavorite((prevState) => !prevState);
-  };
+    setShowFavorite((prevState) => !prevState)
+  }
 
-  const filteredRecipes = showFavorite ? getFavoriteRecipes() : recipes;
+  const filteredRecipes = showFavorite ? getFavoriteRecipes() : recipes
 
   return (
     <div className={styles.app}>
@@ -96,7 +96,7 @@ function App() {
         ))}
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,24 +1,29 @@
-import React from "react";
-import styles from "./Recipe.module.css";
-import heart from "../assets/heart.png";
+import React from 'react'
+import styles from './Recipe.module.css'
+import heart from '../assets/heart.png'
 
 const Recipe = ({ title, image, ingredients, toggleFavorite, bookmarked }) => {
-    const handleFavorite = () => {
-        toggleFavorite(title);
-    };
+  const handleFavorite = () => {
+    toggleFavorite(title)
+  }
 
-    return (
-        <div className={styles.recipe}>
-            <h1 className={styles.title}>{title}</h1>
-            <img className={styles.recipeImage} src={image} alt="" />
-            <ul className={styles.ingredients}>
-                {ingredients.map((ingredient, i) => (
-                    <li key={i}>{ingredient.text}</li>
-                ))}
-            </ul>
-            <img className={`${styles.heart} ${bookmarked ? styles.active : ''}`} src={heart} alt="heart" onClick={handleFavorite} />
-        </div>
-    );
-};
+  return (
+    <div className={styles.recipe}>
+      <h1 className={styles.title}>{title}</h1>
+      <img className={styles.recipeImage} src={image} alt="" />
+      <ul className={styles.ingredients}>
+        {ingredients.map((ingredient, i) => (
+          <li key={i}>{ingredient.text}</li>
+        ))}
+      </ul>
+      <img
+        className={`${styles.heart} ${bookmarked ? styles.active : ''}`}
+        src={heart}
+        alt="heart"
+        onClick={handleFavorite}
+      />
+    </div>
+  )
+}
 
-export default Recipe;
+export default Recipe
